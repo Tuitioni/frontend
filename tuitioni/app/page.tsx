0ef874f5996a9b5e10ebe07f5d230d397877a0e8
codejutsu-desu <mrimmoys@gmail.com>
2024-08-03 22:16:16 +0600

    yt

--- tuitioni/app/page.tsx
+++ tuitioni/app/page.tsx
@@ -1,11 +1,15 @@
 import { Separator } from "@/components/ui/separator";
 import Hero from "@/ui/Hero";
+import VideoPlayer from "@/ui/VideoPlayer";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <Separator className="my-8" />
+      <div className="w-full flex justify-center mb-4 ">
+        <VideoPlayer url="https://www.youtube.com/watch?v=silITbHntzA" />
+      </div>
     </>
   );
 }
