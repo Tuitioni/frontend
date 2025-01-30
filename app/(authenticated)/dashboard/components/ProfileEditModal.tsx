f5c5a282c838061d21cdfc37c3166eeb45188f1a
Rhyme Rubayet <rubayet211@gmail.com>
2025-01-30 17:36:57 +0600

    Hopefully last teacher fix

--- app/(authenticated)/dashboard/components/ProfileEditModal.tsx
+++ app/(authenticated)/dashboard/components/ProfileEditModal.tsx
@@ -8,7 +8,7 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { useState } from "react";
-import { TeacherProfile } from "@/types/Teacher";
+import { TeacherProfile } from "@/types/teacher";
 import { useAuth } from "@/hooks/useAuth";
 import {
   Select,
