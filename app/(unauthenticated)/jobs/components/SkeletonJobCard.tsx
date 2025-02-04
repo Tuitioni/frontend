29a75b28c25e6c7078211b82a5bc5802f72dde7f
codejutsu-desu <mrimmoys@gmail.com>
2025-02-04 15:41:00 +0600

    Add favicon, improve UI components, and enhance page rendering

--- app/(unauthenticated)/jobs/components/SkeletonJobCard.tsx
+++ app/(unauthenticated)/jobs/components/SkeletonJobCard.tsx
@@ -4,15 +4,15 @@ export default function SkeletonJobCard() {
   return (
     <div className="p-3 border rounded-md shadow-md">
       <div className="mb-2">
-        <Skeleton className="h-5 w-3/4 mb-2" />
-        <Skeleton className="h-4 w-full" />
+        <Skeleton className="h-5 w-3/4 mb-2 bg-gray-200" />
+        <Skeleton className="h-4 w-full bg-gray-200" />
       </div>
 
       <div className="grid grid-cols-1 gap-1.5">
-        <Skeleton className="h-4 w-1/2" />
-        <Skeleton className="h-4 w-2/3" />
-        <Skeleton className="h-4 w-1/2" />
-        <Skeleton className="h-4 w-3/4" />
+        <Skeleton className="h-4 w-1/2 bg-gray-200" />
+        <Skeleton className="h-4 w-2/3 bg-gray-200" />
+        <Skeleton className="h-4 w-1/2 bg-gray-200" />
+        <Skeleton className="h-4 w-3/4 bg-gray-200" />
       </div>
     </div>
   );
